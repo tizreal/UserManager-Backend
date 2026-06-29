749c4ec197a80af3a68dd5bd0fc792080dba8ec0
Temiloluwa Odunfa <odunifa.temi@gmail.com>
2026-06-29 00:25:41 -0400

    made changes to allowed url for cors config

--- UserManager/src/main/java/com/tca/UserManager/config/WebSecurityConfig.java
+++ UserManager/src/main/java/com/tca/UserManager/config/WebSecurityConfig.java
@@ -44,7 +44,11 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+        	    "http://localhost:5173",
+        	    "http://localhost:3000",
+        	    "https://usermanager-frontend.vercel.app"
+        	));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
